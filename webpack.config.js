d9cce24a9590435c7992e1c38bfabda8977f3d96
Gabriella Pernama <gabypernama@gmail.com>
2019-09-01 08:32:12 -0700

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./client/src/index.html",
+    output: {
+        path: path.resolve(__dirname, "./client/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+        {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+                presets: ["@babel/preset-react", "@babel/preset-env"]
+        }
+        } 
+    ]
+    }
+}
